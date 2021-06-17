da785130c8c1d5172f8849d72d0a0f767355b3a2
Johann Hemmann <johann.hemmann@code.berlin>
2021-06-17 12:30:23 +0200

    annote request handler

--- api/views.py
+++ api/views.py
@@ -1,8 +1,8 @@
 import json
 
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 
 
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     data = {"msg": "Hello, world."}
     return HttpResponse(json.dumps(data), content_type="application/json")
