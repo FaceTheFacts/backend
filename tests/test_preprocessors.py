ffa83508486e4b2a9ef5f52184e5e31fba06a49b
Johann Hemmann <johann.hemmann@code.berlin>
2021-06-23 23:19:50 +0200

    `tests::test_preprocessors`: Add tests for `preprocess_occupation`, `preprocess_party`

---
+++ tests/test_preprocessors.py
@@ -0,0 +1,35 @@
+import pytest
+
+from backend.preprocessors import preprocess_occupation, preprocess_party
+
+
+@pytest.mark.parametrize(
+    "input,expected",
+    [
+        # Analena Baerbock (79475)
+        ("MdB, Bundesvorsitzende der Grünen", ["MdB", "Bundesvorsitzende"]),
+        # Annegret Kramp-Karrenbauer (138124)
+        ("Ministerpräsidentin des Saarlandes, MdL", ["MdL", "Ministerpräsidentin"]),
+        # Hubertus Heil (79316)
+        (
+            "Bundesminister für Arbeit und Soziales, MdB, stellvertretender Fraktionsvorsitzender",
+            ["MdB", "Bundesarbeitsminister", "Fraktionsvorsitzender"],
+        ),
+    ],
+)
+def test_preprocess_occupation(input: str, expected: list[str]):
+    # `.sort` makes sure we don't care for the order of the result
+    assert preprocess_occupation(input).sort() == expected.sort()
+
+
+@pytest.mark.parametrize(
+    "input,expected",
+    [
+        ("CDU", "CDU"),
+        ("Bündnis 90/Die Grünen", "Die Grünen"),
+        ("Die Linke", "Die Linke"),
+        ("SPD", "SPD"),
+    ],
+)
+def test_preprocess_party(input: str, expected: str):
+    assert preprocess_party(input) == expected
