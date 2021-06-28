c8b0d39eb3541efa17d6a735570489e1a1f0ffd9
Johann Hemmann <johann.hemmann@code.berlin>
2021-06-28 22:52:29 +0200

    Add map of corrected occupations in module `data`

---
+++ data/occupations.py
@@ -0,0 +1,18 @@
+# Map `politician_id` to corrected occupations for some politicians.
+OCCUPATIONS = {
+    # Olaf Scholz
+    66924: ["Finanzminister", "Kanzlerkandidat"],
+    # Karl Lauterbach
+    79215: ["MdB"],
+    # Christian Lindner
+    119742: ["MdB", "Fraktionsvorsitzender", "Parteivorsitzender"],
+    # Armin Laschet
+    130072: [
+        "MdL",
+        "Ministerpräsident NRW",
+        "Parteivorsitzender",
+        "Kanzlerkandidat",
+    ],
+    # Robert Habeck
+    139064: ["Parteivorsitzender", "Schriftstelller"],
+}
