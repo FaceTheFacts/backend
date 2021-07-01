5e7b990a2ff7ad6a5377662f21c2fef445a709aa
Fingolf6455 <72171703+Fingolf6455@users.noreply.github.com>
2021-07-01 19:56:24 +0200

    Update occupations.py

--- data/occupations.py
+++ data/occupations.py
@@ -15,4 +15,34 @@ OCCUPATIONS = {
     ],
     # Robert Habeck
     139064: ["Parteivorsitzender", "Schriftstelller"],
+    # Julia Klöckner
+    110066:["Bundesernährungsministerin"],
+    # Friedrich Merz
+    118559:[],
+    # Horst Seehofer
+    118761: ["Bundesinnenminister"],
+    # Heiko Maas
+    138233: ["MdB", "Bundesaußenminister"],
+    # Peter Altmaier
+    78815: ["MdB", "Bundeswirtschaftsminister"],
+    # Christine Lambrecht
+    79208: ["MdB", "Bundesjustizministerin", "Bundesfamilienministerin"],
+    # Michael Müller
+    117519: ["MdA", "Landesvorsitzender", "Regierender Bürgermeister"],
+    # Kramp-Karrenbauer
+    138124: ["Bundesverteidigungsministerin", "Ministerpräsidentin a.D."],
+    # Julia Klöckner
+    110066: ["Bundesernährungsministerin"],
+    # Hubertus Heil
+    79316: ["Bundesarbeitsminister", "stellv. Fraktionsvorsitzender"],
+    # Annaena Baerbock
+    79475: ["MdB", "Parteivorsitzende"],
+    # Svenja Schulze
+    131019: ["Bundesumweltministerin"],
+    # Anja Karliczek
+    79249: ["MdB", "Bundesbildungsministerin"],
+    # Gerd Müller
+    79121: ["MdB", "Bundesentwicklungsminister"],
+    # Helge Braun
+    79432: ["MdB", "Bundeskanzleramtschef"],    
 }
