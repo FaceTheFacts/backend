50efa0098b1e88fc64edc990959266ff6a990937
Johann Hemmann <johann.hemmann@code.berlin>
2021-07-08 17:17:49 +0200

    Sort by key

--- data/occupations.py
+++ data/occupations.py
@@ -2,8 +2,30 @@
 OCCUPATIONS = {
     # Olaf Scholz
     66924: ["Finanzminister", "Kanzlerkandidat"],
+    # Peter Altmaier
+    78815: ["MdB", "Bundeswirtschaftsminister"],
+    # Gerd Müller
+    79121: ["MdB", "Bundesentwicklungsminister"],
+    # Christine Lambrecht
+    79208: ["MdB", "Bundesjustizministerin", "Bundesfamilienministerin"],
     # Karl Lauterbach
     79215: ["MdB"],
+    # Anja Karliczek
+    79249: ["MdB", "Bundesbildungsministerin"],
+    # Hubertus Heil
+    79316: ["Bundesarbeitsminister", "stellv. Fraktionsvorsitzender"],
+    # Helge Braun
+    79432: ["MdB", "Bundeskanzleramtschef"],
+    # Annaena Baerbock
+    79475: ["MdB", "Parteivorsitzende"],
+    # Julia Klöckner
+    110066: ["Bundesernährungsministerin"],
+    # Michael Müller
+    117519: ["MdA", "Landesvorsitzender", "Regierender Bürgermeister"],
+    # Friedrich Merz
+    118559: [],
+    # Horst Seehofer
+    118761: ["Bundesinnenminister"],
     # Christian Lindner
     119742: ["MdB", "Fraktionsvorsitzender", "Parteivorsitzender"],
     # Armin Laschet
@@ -13,36 +35,12 @@ OCCUPATIONS = {
         "Parteivorsitzender",
         "Kanzlerkandidat",
     ],
-    # Robert Habeck
-    139064: ["Parteivorsitzender", "Schriftstelller"],
-    # Julia Klöckner
-    110066: ["Bundesernährungsministerin"],
-    # Friedrich Merz
-    118559: [],
-    # Horst Seehofer
-    118761: ["Bundesinnenminister"],
-    # Heiko Maas
-    138233: ["MdB", "Bundesaußenminister"],
-    # Peter Altmaier
-    78815: ["MdB", "Bundeswirtschaftsminister"],
-    # Christine Lambrecht
-    79208: ["MdB", "Bundesjustizministerin", "Bundesfamilienministerin"],
-    # Michael Müller
-    117519: ["MdA", "Landesvorsitzender", "Regierender Bürgermeister"],
-    # Kramp-Karrenbauer
-    138124: ["Bundesverteidigungsministerin", "Ministerpräsidentin a.D."],
-    # Julia Klöckner
-    110066: ["Bundesernährungsministerin"],
-    # Hubertus Heil
-    79316: ["Bundesarbeitsminister", "stellv. Fraktionsvorsitzender"],
-    # Annaena Baerbock
-    79475: ["MdB", "Parteivorsitzende"],
     # Svenja Schulze
     131019: ["Bundesumweltministerin"],
-    # Anja Karliczek
-    79249: ["MdB", "Bundesbildungsministerin"],
-    # Gerd Müller
-    79121: ["MdB", "Bundesentwicklungsminister"],
-    # Helge Braun
-    79432: ["MdB", "Bundeskanzleramtschef"],
+    # Kramp-Karrenbauer
+    138124: ["Bundesverteidigungsministerin", "Ministerpräsidentin a.D."],
+    # Heiko Maas
+    138233: ["MdB", "Bundesaußenminister"],
+    # Robert Habeck
+    139064: ["Parteivorsitzender", "Schriftstelller"],
 }
