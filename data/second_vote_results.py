c35824cb2da93e0aec30965ce0b480958eddb1e7
Richard <richard.kruemmel@code.berlin>
2021-07-19 12:21:27 +0200

    Implement Second_Vote_Results data

---
+++ data/second_vote_results.py
@@ -0,0 +1,18 @@
+SECOND_VOTE_RESULTS = {
+    "Baden-Würtenberg": {"CDU": 0, "SPD": 16, "Die Grünen": 13, "FDP": 12, "AFD": 11, "Die Linke": 6},
+    "Bayern": {"CSU": 0, "SPD": 18, "Die Grünen": 11, "FDP": 12, "AFD": 14, "Die Linke": 7},
+    "Berlin": {"CDU": 2, "SPD": 4, "Die Grünen": 3, "FDP": 3, "AFD": 4, "Die Linke": 5},
+    "Brandenburg": {"CDU": 0, "SPD": 4, "Die Grünen": 1, "FDP": 2, "AFD": 5, "Die Linke": 4},
+    "Bremen": {"CDU": 1, "SPD": 0, "Die Grünen": 1, "FDP": 0, "AFD": 1, "Die Linke": 1},
+    "Hamburg": {"CDU": 3, "SPD": 0, "Die Grünen": 2, "FDP": 2, "AFD": 1, "Die Linke": 2},
+    "Hessen": {"CDU": 0, "SPD": 9, "Die Grünen": 5, "FDP": 6, "AFD": 6, "Die Linke": 4},
+    "Mecklenburg-Vorpommern": {"CDU": 0, "SPD": 2, "Die Grünen": 1, "FDP": 1, "AFD": 3, "Die Linke": 3},
+    "Niedersachsen": {"CDU": 9, "SPD": 10, "Die Grünen": 6, "FDP": 7, "AFD": 7, "Die Linke": 5},
+    "Nordrhein-Westfalen": {"CDU": 10, "SPD": 17, "Die Grünen": 12, "FDP": 20, "AFD": 12, "Die Linke": 15},
+    "Rheinland-Pfalz": {"CDU": 0, "SPD": 9, "Die Grünen": 3, "FDP": 4, "AFD": 4, "Die Linke": 3},
+    "Saarland": {"CDU": 0, "SPD": 3, "Die Grünen": 1, "FDP": 1, "AFD": 1, "Die Linke": 1},
+    "Sachsen": {"CDU": 0, "SPD": 4, "Die Grünen": 2, "FDP": 3, "AFD": 11, "Die Linke": 5},
+    "Sachsen-Anhalt": {"CDU": 0, "SPD": 3, "Die Grünen": 1, "FDP": 2, "AFD": 4, "Die Linke": 4},
+    "Schleswig-Holstein": {"CDU": 0, "SPD": 5, "Die Grünen": 3, "FDP": 3, "AFD": 2, "Die Linke": 2},
+    "Thüringen": {"CDU": 0, "SPD": 3, "Die Grünen": 1, "FDP": 2, "AFD": 5, "Die Linke": 3}
+}
