b06303ee7f8ecf51bfc7e2047078f4c3e9787cf9
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-09-22 12:29:17 +0200

    Add Base

--- database/models.py
+++ database/models.py
@@ -1,6 +1,8 @@
 from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
+Base = declarative_base()
 
 class Politician(Base):
     __tablename__ = "politician"
@@ -14,7 +16,7 @@ class Politician(Base):
 class Party(Base):
     __tablename__ = "party"
     id = Column(Integer, primary_key=True, index=True)
-    displayName = Column(String, nullable=False)
-    foregroundColor = Column(String, nullable=False)
-    backgroundColor = Column(String, nullable=False)
-    borderColor = Column(String)
+    display_name = Column(String, nullable=False)
+    foreground_color = Column(String, nullable=False)
+    background_color = Column(String, nullable=False)
+    border_color = Column(String)
