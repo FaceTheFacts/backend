0301206669b66f3a104b0c3f819bdc0102731b63
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-10-13 11:33:09 +0200

    Define Sidejob schema

--- schemas.py
+++ schemas.py
@@ -55,3 +55,20 @@ class Politician(BaseModel):
 
     class Config:
         orm_mode = True
+
+
+class Sidejob(BaseModel):
+    id: int
+    entity_type: str
+    label: str
+    api_url: str
+    job_title_extra: Optional[str]
+    additional_information: Optional[str]
+    category: str
+    income_level: Optional[str]
+    interval: Optional[str]
+    data_change_date: date
+    created: int
+    # sidejob_organization_id:int, ForeignKey("sidejob_organization.id"
+    # field_city_id:int, ForeignKey("city.id"
+    # field_country_id:int, ForeignKey("country.id"
