05ee4d0a961b28a1d9c81a07d848cb7104a26d33
Amit Amanna <amit.natheniel@gmail.com>
2021-10-13 11:00:11 +0200

    Added models for Candidacy_mandate, Electoral_data and Constituency

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer, ForeignKey, Text, Date, Boolean
+from sqlalchemy import Column, String, Integer, ForeignKey, Text, Date, Boolean, Float
 from sqlalchemy.orm import relationship
 
 from database import Base
@@ -59,9 +59,83 @@ class Politician(Base):
     qid_wikidata = Column(String)
     field_title = Column(String)
 
-    # Many to One
+    # # Many to One
     # party = relationship("Party")
 
-    # One to Many
-    # candidacy_mandates = relationship("CandidacyMandate", back_populates="politician")
+    # # One to Many
+    candidacy_mandates = relationship("Candidacy_mandate", back_populates="politician")
     # positions = relationship("Position", back_populates="politician")
+
+
+class Candidacy_mandate(Base):
+    __tablename__ = "candidacy_mandate"
+    id = Column(Integer, primary_key=True)
+    entity_type = Column(String)
+    label = Column(String)
+    api_url = Column(String)
+    id_external_administration = Column(String)
+    id_external_administration_description = Column(String)
+    type = Column(String)
+    # parliament_period_id = Column(Integer, ForeignKey("parliament_period.id"))
+    # politician_id = Column(Integer, ForeignKey("politician.id"))
+    # party_id = Column(Integer, ForeignKey("party.id"))
+    start_date = Column(Date)
+    end_date = Column(Date)
+    info = Column(String)
+    electoral_data_id = Column(Integer, ForeignKey("electoral_data.id"))
+    # fraction_membership_id = Column(Integer, ForeignKey("fraction_membership.id"))
+
+    # # Many to One
+    # parliament_period = relationship(
+    #     "Parliament_period", back_populates="candidacy_mandates"
+    # )
+    # politician = relationship("Politician", back_populates="candidacy_mandates")
+    # party = relationship("Party", back_populates="candidacy_mandates")
+    # # One to One
+    electoral_data = relationship(
+        "Electoral_data", back_populates="candidacy_mandate", uselist=False
+    )
+    # fraction_membership = relationship(
+    #     "Fraction_membership", back_populates="candidacy_mandate", uselist=False
+    # )
+    # # One to Many
+    # committee_memberships = relationship(
+    #     "Committee_membership", back_populates="candidacy_mandate"
+    # )
+    # votes = relationship("Vote", back_populates="candidacy_mandate")
+    # # Many to Many
+    # sidejobs = relationship(
+    #     "Sidejob",
+    #     secondary="sidejob_has_mandate",
+    #     back_populates="candidacy_mandates",
+    # )
+
+    class Electoral_data(Base):
+        __tablename__ = "electoral_data"
+        id = Column(Integer, primary_key=True)
+        entity_type = Column(String)
+        label = Column(String)
+        # electoral_list_id = Column(Integer, ForeignKey("electoral_list.id"))
+        list_position = Column(Integer)
+        constituency_id = Column(Integer, ForeignKey("constituency.id"))
+        constituency_result = Column(Float)
+        constituency_result_count = Column(Integer)
+        mandate_won = Column(String)
+        # electoral_list = relationship("Electoral_list", back_populates="electoral_data")
+        constituency = relationship("Constituency", back_populates="electoral_data")
+        # One to One
+        candidacy_mandate = relationship(
+            "Candidacy_mandate", back_populates="electoral_data"
+        )
+
+    class Constituency(Base):
+        __tablename__ = "constituency"
+        id = Column(Integer(), primary_key=True)
+        entity_type = Column(String)
+        label = Column(String)
+        api_url = Column(String)
+        name = Column(String)
+        number = Column(Integer)
+        # parliament_period_id = Column(Integer, ForeignKey("parliament_period.id"))
+        # parliament_period = relationship("Parliament_period")
+        electoral_data = relationship("Electoral_data", back_populates="constituency")
