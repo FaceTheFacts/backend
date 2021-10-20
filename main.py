026b002c2d4803e05787607230c69c408c658a5f
Amit Amanna <amit.natheniel@gmail.com>
2021-10-20 11:30:05 +0200

    changed endpoint function name

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ def read_politician_search(text: str, db: Session = Depends(get_db)):
 
 
 @app.get("/image-scanner", response_model=Page[schemas.PoliticianName])
-def get_politicians_by_partial_name(text: str, db: Session = Depends(get_db)):
+def read_politicians_image_scanner(text: str, db: Session = Depends(get_db)):
     politician = crud.get_politician_by_search(db, text)
     return paginate(politician)
 
