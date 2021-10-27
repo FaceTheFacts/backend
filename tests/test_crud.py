1fffb7b7224c1d2ce3630ed113053e5efd21824c
Amit Amanna <amit.natheniel@gmail.com>
2021-10-27 16:24:13 +0200

    fixed test_crud tests

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -10,15 +10,10 @@ from main import get_db
 class TestGetPoliticianBySearch(unittest.TestCase):
     @patch('crud.get_politicians_by_partial_name')
     def test__get_politicians_by_partial_name__is_called(self, mock):
-        get_politician_by_search(Depends(get_db), "SomeText")
+        get_politician_by_search(Depends(get_db), "Marie")
         self.assertTrue(mock.called)
 
-    @patch('crud.get_politicians_by_partial_name')
-    def test__get_politicians_by_partial_name__not_called(self, mock):
-        get_politician_by_search(Depends(get_db), "12345")
-        self.assertFalse(mock.called)
-
-    @patch('crud.get_politicians_by_partial_zipcode')
+    @patch('crud.get_politicians_by_zipcode')
     def test__get_politicians_by_partial_zipcode__is_called(self, mock):
-        get_politician_by_search(Depends(get_db), "12345")
+        get_politician_by_search(Depends(get_db), "54340")
         self.assertTrue(mock.called)
