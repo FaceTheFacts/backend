9617da38ac975949d616cd48b5a40bdf7b86375d
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-11-05 16:55:38 +0100

    Write append functions and tests for append_sidejobs

---
+++ tests/cron_jobs/test_append_db.py
@@ -0,0 +1,26 @@
+# local
+from src.cron_jobs.append_db import append_sidejobs
+from src.db.models.sidejob import Sidejob
+from src.db.connection import Session
+import src.db.models as models
+
+# third-party
+from sqlalchemy import delete
+
+session = Session()
+
+
+def test_append_sidejobs():
+    def drop_last_item_and_update():
+        last_id = (
+            session.query(models.Sidejob).order_by(models.Sidejob.id.desc()).first().id
+        )
+        stmt = delete(Sidejob).where(Sidejob.id == last_id)
+        session.execute(stmt)
+        session.commit()
+        session.close()
+        assert append_sidejobs()[0]["label"] == 'Mitglied des Stiftungsrates, ehrenamtlich'
+        assert append_sidejobs()[0]["id"] == 11699
+        
+
+    drop_last_item_and_update()
