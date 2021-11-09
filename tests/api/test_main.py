a035d40c536bf78dcb78076ae00231146fc413b1
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-11-09 12:11:51 +0100

    Delete Poll route and tests for it

--- tests/api/test_main.py
+++ tests/api/test_main.py
@@ -1,50 +1,7 @@
 from fastapi.testclient import TestClient
-import json
 from src.api.main import app
 
 client = TestClient(app)
-
-
-def test_read_poll():
-    def all_elements_have_values():
-        response = client.get("/poll/1126")
-        assert response.status_code == 200
-        assert type(response.json()) is dict
-        assert response.json() == {
-            "id": 1126,
-            "entity_type": "node",
-            "label": "Auszahlung von Griechenlandhilfen",
-            "committee": {
-                "id": 480,
-                "entity_type": "node",
-                "label": "Ausschuss für Regionale Entwicklung",
-            },
-            "field_intro": '<p>\r\n\tDas Europäische Parlament hat der <a href="http://www.europarl.europa.eu/sides/getDoc.do?pubRef=-//EP//NONSGML+TA+P8-TA-2015-0332+0+DOC+PDF+V0//DE">zügigen Umsetzung</a> des Wachstums- und Beschäftigungsplans für Griechenland zugestimmt.\r\n</p>\r\n\r\n<p>\r\n\t<strong>Bitte beachten Sie, dass wir nur das Abstimmungsergebnis für die deutschen EU-Abgeordneten darstellen.</strong>\r\n</p>\r\n',
-            "field_poll_date": "2015-10-06",
-        }
-
-    def committee_id_null():
-        response = client.get("/poll/643")
-        assert response.status_code == 200
-        assert type(response.json()) is dict
-        assert response.json() == {
-            "id": 643,
-            "entity_type": "node",
-            "label": "Veränderung der Volksgesetzgebung",
-            "committee": None,
-            "field_intro": "Mit 60 zu 56 Stimmen hat die Bürgerschaft eine Veränderung der Volksgesetzgebung beschlossen. Die CDU votierte einstimmig für den Gesetzesentwurf, SPD und GAL dagegen. Der Beschluss sieht u.a. vor, dass das Sammeln von Unterschriften für ein Volksbegehren künftig in Ämtern erfolgen muss und nicht mehr auf der Straße.",
-            "field_poll_date": "2005-04-14",
-        }
-
-    def poll_id_not_found():
-        response = client.get("/poll/1")
-        assert response.status_code == 404
-        assert type(response.json()) is dict
-        assert response.json() == {"detail": "Poll not found"}
-
-    all_elements_have_values()
-    committee_id_null()
-    poll_id_not_found()
 
 
 def test_read_politician():
