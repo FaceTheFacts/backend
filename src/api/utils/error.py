0f4d72b5e4fc40b491a36ccb5f848ea4b34b7721
AmNat789 <amit.natheniel@gmail.com>
2021-11-23 15:13:17 +0100

    fixed check_entity_not_found based on test

--- src/api/utils/error.py
+++ src/api/utils/error.py
@@ -2,5 +2,5 @@ from fastapi import HTTPException
 
 
 def check_entity_not_found(entity, stringified_entity: str):
-    if entity is None:
+    if not entity:
         raise HTTPException(status_code=404, detail=f"{stringified_entity} not found")
--- tests/api/utils/test_error.py
+++ tests/api/utils/test_error.py
@@ -1,20 +1,25 @@
 from src.api.utils.error import check_entity_not_found
 from fastapi import HTTPException
 
+
 def test_check_entity_not_found():
     def pass_None_test():
         try:
             check_entity_not_found(None, "Entity")
-            assert False, "check_entity_not_found does not throw HTTPException when None is passed as entity"
+            assert (
+                False
+            ), "check_entity_not_found does not throw HTTPException when None is passed as entity"
         except HTTPException:
             assert True
-
 
     def pass_empty_list_test():
         try:
             check_entity_not_found([], "Entity")
-            assert False, "check_entity_not_found does not throw HTTPException when empty list is passed as entity"
+            assert (
+                False
+            ), "check_entity_not_found does not throw HTTPException when empty list is passed as entity"
         except HTTPException:
             assert True
+
     pass_None_test()
     pass_empty_list_test()
