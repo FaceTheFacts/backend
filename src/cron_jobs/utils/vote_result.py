652a61f20982c5a424fb4273107b59f29c74ba86
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-11-23 17:39:37 +0100

    Refactor generate_vote_results func

--- src/cron_jobs/utils/vote_result.py
+++ src/cron_jobs/utils/vote_result.py
@@ -10,6 +10,21 @@ def read_poll_vote_result_df() -> DataFrame:
     return df
 
 
+def is_exist_vote_result(df: DataFrame, poll_id: int, vote: str) -> bool:
+    if df[(df.poll_id == poll_id) & (df.vote == vote)]["count"].values.size > 0:
+        return True
+    else:
+        return False
+
+
+def get_vote_result(df: DataFrame, poll_id: int, vote: str) -> any:
+    is_exist_result = is_exist_vote_result(df, poll_id, vote)
+    if is_exist_result == False:
+        return 0
+    else:
+        return df[(df.poll_id == poll_id) & (df.vote == vote)]["count"].values[0]
+
+
 def generate_vote_results():
     data_list = []
     df = read_poll_vote_result_df()
@@ -18,37 +33,12 @@ def generate_vote_results():
         if poll_id != None:
             new_dict = {
                 "poll_id": poll_id,
-                "yes": df[(df.poll_id == poll_id) & (df.vote == "yes")]["count"].values[
-                    0
-                ]
-                if df[(df.poll_id == poll_id) & (df.vote == "yes")]["count"].values.size
-                > 0
-                else 0,
-                "no": df[(df.poll_id == poll_id) & (df.vote == "no")]["count"].values[0]
-                if df[(df.poll_id == poll_id) & (df.vote == "no")]["count"].values.size
-                > 0
-                else 0,
-                "abstain": df[(df.poll_id == poll_id) & (df.vote == "abstain")][
-                    "count"
-                ].values[0]
-                if df[(df.poll_id == poll_id) & (df.vote == "abstain")][
-                    "count"
-                ].values.size
-                > 0
-                else 0,
-                "no_show": df[(df.poll_id == poll_id) & (df.vote == "no_show")][
-                    "count"
-                ].values[0]
-                if df[(df.poll_id == poll_id) & (df.vote == "no_show")][
-                    "count"
-                ].values.size
-                > 0
-                else 0,
+                "yes": get_vote_result(df, poll_id, "yes"),
+                "no": get_vote_result(df, poll_id, "no"),
+                "abstain": get_vote_result(df, poll_id, "abstain"),
+                "no_show": get_vote_result(df, poll_id, "no_show"),
             }
         data_list.append(new_dict)
 
     return data_list
 
-
-if __name__ == "__main__":
-    print(generate_vote_results())
