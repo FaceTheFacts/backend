2d8260d02c2e13578d694feaf12a219137880bd0
AmNat789 <amit.natheniel@gmail.com>
2021-11-25 14:06:25 +0100

    created PollResultPerParty class.

---
+++ src/db/models/poll_result_per_party.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+
+from src.db.connection import Base
+
+
+class PollResultPerParty(Base):
+    __tablename__ = "poll_result_per_party"
+
+    id = Column(Integer(), primary_key=True)
+    entity_type = Column(String)
+    poll_id = Column(Integer, ForeignKey("poll.id"))
+    party_id = Column(Integer, ForeignKey("party.id"))
+    total_yes = Column(Integer)
+    total_no = Column(Integer)
+    total_abstain = Column(Integer)
+    total_no_show = Column(Integer)
+
+
+    # Many to One
+    poll = relationship("Poll", back_populates="poll_result_per_party")
+    party = relationship("Party", back_populates="poll_result_per_party")
