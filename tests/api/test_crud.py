a366c57b325074ab16b25d95bdf0c7c21484afea
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-12-01 11:54:18 +0100

    Add a mock up database

--- tests/api/test_crud.py
+++ tests/api/test_crud.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest import mock
 from unittest.mock import patch
 
 from fastapi import Depends
@@ -17,3 +18,72 @@ class TestGetPoliticianBySearch(unittest.TestCase):
     def test__get_politicians_by_partial_zipcode__is_called(self, mock):
         get_politician_by_search(Depends(get_db), "54340")
         self.assertTrue(mock.called)
+
+
+from mock_alchemy.mocking import UnifiedAlchemyMagicMock
+import src.db.models as models
+
+# session = UnifiedAlchemyMagicMock()
+# session.add(models.Politician(id=1, label='bar'))
+# session.add(models.Politician(id=2, label='baz'))
+
+# session.add(models.CandidacyMandate(id=1, politician_id=1))
+# session.add(models.CandidacyMandate(id=2, politician_id=2))
+# session.add(models.SidejobHasMandate(sidejob_id=1, candidacy_mandate_id=1))
+# session.add(models.SidejobHasMandate(sidejob_id=2, candidacy_mandate_id=2))
+# session.add(models.Sidejob(id=1, label="example1"))
+# session.add(models.Sidejob(id=2, label="example2"))
+
+
+session = UnifiedAlchemyMagicMock(
+    data=[
+        (
+            [
+                mock.call.query(models.Politician),
+                mock.call.filter(models.Politician.id == 1),
+            ],
+            [models.Politician(id=1, label="bar")],
+        ),
+        (
+            [
+                mock.call.query(models.Politician),
+                mock.call.filter(models.Politician.id == 2),
+            ],
+            [models.Politician(id=2, label="baz")],
+        ),
+        (
+            [
+                mock.call.query(models.CandidacyMandate),
+                mock.call.filter(
+                    models.Politician.id == 1,
+                    models.Politician.id == models.CandidacyMandate.politician_id,
+                ),
+            ],
+            [models.CandidacyMandate(id=1, politician_id=1, label="example1")],
+        ),
+        (
+            [
+                mock.call.query(models.CandidacyMandate),
+                mock.call.filter(
+                    models.Politician.id == 2,
+                    models.Politician.id == models.CandidacyMandate.politician_id,
+                ),
+            ],
+            [models.CandidacyMandate(id=2, politician_id=2, label="example2")],
+        ),
+    ]
+)
+
+
+def function1(session, id):
+
+    return (
+        session.query(models.CandidacyMandate)
+        .filter(models.Politician.id == id)
+        .filter(models.Politician.id == models.CandidacyMandate.politician_id)
+        .all()
+    )
+
+
+x = function1(session, 2)
+print(x[0].label)
