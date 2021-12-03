fc639564cb30a361c55f77c849f9baf212e7b530
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-12-03 13:07:30 +0100

    Write a unittest for the politician_sidejobs route

---
+++ tests/api/versions/test_mockup_v1.py
@@ -0,0 +1,60 @@
+# local
+from src.api.main import app
+from tests.db.mock_up_database import mockup_session
+import src.db.models as models
+
+# default
+import unittest
+from unittest.mock import patch
+
+# Third-party
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+
+class TestV1Routes(unittest.TestCase):
+    # unittest
+    @patch(
+        "src.db.connection.Session",
+        return_value=mockup_session,
+    )
+    @patch(
+        "src.api.crud.get_sidejobs_by_politician_id",
+        return_value=[
+            models.Sidejob(
+                id=1,
+                entity_type="sidejob",
+                label="Member of the County Council",
+                income_level="1.000 € bis 3.500 €",
+                data_change_date="2021-09-10",
+            ),
+            models.Sidejob(
+                id=2,
+                entity_type="sidejob",
+                label="Chairman",
+                income_level="3.500 € bis 7.000 €",
+                data_change_date="2021-09-10",
+            ),
+        ],
+    )
+    def test_read_politician_sidejobs(self, session, crud):
+        response = client.get("/v1/politician/1/sidejobs?page=1&size=1")
+        assert response.status_code == 200
+        expected = [
+            {
+                "id": 1,
+                "entity_type": "sidejob",
+                "label": "Member of the County Council",
+                "income_level": "1.000 € bis 3.500 €",
+                "interval": None,
+                "data_change_date": "2021-09-10",
+                "sidejob_organization": None,
+            }
+        ]
+
+        self.assertEqual(response.json()["items"], expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
