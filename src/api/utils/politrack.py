e1a3f4fee84741b1c26faf020ff8ab8da56d4a75
AmNat789 <amit.natheniel@gmail.com>
2021-12-15 11:57:08 +0100

    added function to generate auth headers for politrack api

---
+++ src/api/utils/politrack.py
@@ -0,0 +1,23 @@
+import os
+
+import requests
+
+
+def generate_authenticated_header():
+    def authenticate():
+        resp = requests.post(
+            os.environ["API_URL"] + "/auth",
+            json={
+                "username": os.environ["POLITRACK_USERNAME"],
+                "password": os.environ["POLITRACK_SECRET_PASSWORD"],
+            },
+        )
+        if resp.status_code == 200:
+            return resp.json()["access_token"]
+        print(resp.status, resp.text)
+        return None
+
+    token = authenticate()
+    if token is None:
+        raise RuntimeError("Could not authenticate")
+    return {"Authorization": "Bearer " + token}
