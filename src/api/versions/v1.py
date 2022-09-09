07ac7740e57ef5c7f9835c696f281b867637e5b8
Richard <richard.kruemmel@code.berlin>
2022-09-09 22:28:03 +0200

    Add authenticate and login function

--- src/api/versions/v1.py
+++ src/api/versions/v1.py
@@ -1,23 +1,28 @@
 # std
 import os
 from typing import List
+from datetime import timedelta
 
 # third-party
 import requests
-
-from fastapi import Depends, Query, APIRouter, HTTPException
+from jose import jwt, JWTError
+from fastapi import Depends, Response, Query, APIRouter, HTTPException, status
 from fastapi_pagination import Page, add_pagination, paginate
+from fastapi.security import OAuth2PasswordRequestForm
 
 # local
 import src.api.crud as crud
 import src.api.schemas as schemas
 from src.api.utils import politrack
 from src.api.utils.politician import get_politician_profile
+from src.api.utils.oauth2bearer import OAuth2PasswordBearerWithCookie
 from src.db import models
+from src.api.security import create_access_token
 from src.db.connection import Session
 from src.api.utils.error import check_entity_not_found
 from src.api.utils.party_sort import party_sort
 from src.api.utils.polls import get_politcian_ids_by_bundestag_polldata_and_follow_ids
+from src.api.hashing import Hasher
 
 router = APIRouter(
     prefix="/v1",
@@ -33,6 +38,66 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+def authenticate_user(username: str, password: str, db: Session):
+    print(username)
+    user = crud.get_user(username=username, db=db)
+    if not user:
+        return False
+    if not Hasher.verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+@router.post("/token", response_model=schemas.Token)
+def login_for_access_token(
+    response: Response,
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db),
+):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+        )
+    access_token_expires = timedelta(
+        minutes=int(os.environ["ACCESS_TOKEN_EXPIRE_MINUTES"])
+    )
+    access_token = create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+    response.set_cookie(
+        key="access_token", value=f"Bearer {access_token}", httponly=True
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+oauth2_scheme = OAuth2PasswordBearerWithCookie(tokenUrl="/login/token")
+
+
+def get_current_user_from_token(
+    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+    )
+    try:
+        payload = jwt.decode(
+            token, os.environ["SECRET_KEY"], algorithms=[os.environ["ALGORITHM"]]
+        )
+        username: str = payload.get("sub")
+        print("username/email extracted is ", username)
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = crud.get_user(username=username, db=db)
+    if user is None:
+        raise credentials_exception
+    return user
 
 
 @router.get("/politician/{id}", response_model=schemas.Politician)
