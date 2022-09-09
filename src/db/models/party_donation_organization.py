3eb184496bc90656fcd9f1081cf6d817d4a8ad60
Matias <matias.cardone@code.berlin>
2022-09-09 15:07:30 +0200

    Change in relationship btwn party_donation and party_donation_organization

--- src/db/models/party_donation_organization.py
+++ src/db/models/party_donation_organization.py
@@ -10,11 +10,12 @@ class PartyDonationOrganization(Base):
     id = Column(Integer, primary_key=True)
     donor_name = Column(String)
     donor_address = Column(String)
-    donor_zip = Column(Integer)
+    donor_zip = Column(String)
     donor_city = Column(String)
     donor_foreign = Column(Boolean)
-    party_donation_id = Column(Integer,ForeignKey("party_donation.id"))
 
-    # Many to One (Many donations to one org)
-    party_donation = relationship(
+    # One to Many
+    party_donations = relationship(
         "PartyDonation", back_populates="party_donation_organization")
+
+    
