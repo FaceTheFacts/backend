1dbfe8ce3c22bd1e8b60054a47c2e6ea84072646
Richard <richard.kruemmel@code.berlin>
2023-03-16 18:42:17 +0100

    Add to_dict logic for serialization for caching

--- src/db/models/politician.py
+++ src/db/models/politician.py
@@ -30,6 +30,33 @@ class Politician(Base):
     field_title = Column(String)
     party = relationship("Party")
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "entity_type": self.entity_type,
+            "label": self.label,
+            "api_url": self.api_url,
+            "abgeordnetenwatch_url": self.abgeordnetenwatch_url,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "birth_name": self.birth_name,
+            "sex": self.sex,
+            "year_of_birth": self.year_of_birth,
+            "party_id": self.party_id,
+            "party_past": self.party_past,
+            "deceased": self.deceased,
+            "deceased_date": self.deceased_date,
+            "education": self.education,
+            "residence": self.residence,
+            "occupation": self.occupation,
+            "statistic_questions": self.statistic_questions,
+            "statistic_questions_answered": self.statistic_questions_answered,
+            "qid_wikidata": self.qid_wikidata,
+            "field_title": self.field_title,
+            "party": self.party.to_dict() if self.party else None,
+            # Add other relationships if necessary
+        }
+
     # One to Many
     candidacy_mandates = relationship("CandidacyMandate", back_populates="politician")
     positions = relationship("Position", back_populates="politicians")
