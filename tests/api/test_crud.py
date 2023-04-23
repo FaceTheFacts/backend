70e77943bc4d37895c0156fba1327bf476227116
romeonicholas <79602065+romeonicholas@users.noreply.github.com>
2023-04-23 16:48:25 +0200

    Add test for value errors in time ranges in party donations query

--- tests/api/test_crud.py
+++ tests/api/test_crud.py
@@ -3,7 +3,10 @@ from unittest.mock import patch
 
 from fastapi import Depends
 
+import datetime
+
 from src.api.crud import get_politician_by_search
+from src.api.crud import get_party_donations_for_ids_and_time_range
 from src.db.connection import Session
 
 session = Session()
@@ -19,3 +22,16 @@ class TestGetPoliticianBySearch(unittest.TestCase):
     def test__get_politicians_by_partial_zipcode__is_called(self, mock):
         get_politician_by_search(Depends(session), "54340")
         self.assertTrue(mock.called)
+
+
+class TestGePartyDonationsForIdsAndTimeRange(unittest.TestCase):
+    def get_party_donations_for_ids_and_time_range_invalid_time_range(self):
+        # query_result = get_party_donations_for_ids_and_time_range(Session, [1], datetime.datetime.now(), datetime.datetime.min)
+        self.assertRaises(
+            ValueError,
+            get_party_donations_for_ids_and_time_range,
+            Session,
+            [1, 2, 3, 4, 5, 8, 9, 145],
+            datetime.datetime.now(),
+            datetime.datetime.min,
+        )
