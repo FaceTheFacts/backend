1feb6e2f56166b68e078ced1111d0f591d20e71a
Richard <richard.kruemmel@code.berlin>
2023-09-28 11:56:12 +0200

    Create get last day of the month util function

---
+++ src/api/utils/date_utils.py
@@ -0,0 +1,16 @@
+import datetime
+
+def get_last_day_of_the_month():
+    last_day_of_the_month = None
+
+    today = datetime.date.today()
+    tomorrow = today + datetime.timedelta(days=1)
+
+    if tomorrow.month != today.month:
+        last_day_of_the_month = today
+    else:
+        last_day_of_the_month = datetime.date(
+            today.year, today.month, 1
+        ) - datetime.timedelta(days=1)
+
+    return last_day_of_the_month
