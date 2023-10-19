a5b23eaacd4898bbcee8d6c579f48ebe28750b22
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2023-10-19 10:14:29 +0200

    feat: Set up env for testing

--- src/db/connection.py
+++ src/db/connection.py
@@ -10,14 +10,36 @@ from sqlalchemy.ext.declarative import declarative_base
 
 load_dotenv()
 
-connection_uri = sa.engine.URL.create(
-    "postgresql+psycopg2",
-    username=os.environ["DATABASE_USER"],
-    password=os.environ["DATABASE_PASSWORD"],
-    host=os.environ["DATABASE_HOST"],
-    database=os.environ["DATABASE_NAME"],
-)
+database_connections = {
+    "testing": {
+        "database_user": os.environ["TESTING_DATABASE_USER"],
+        "database_password": os.environ["TESTING_DATABASE_PASSWORD"],
+        "database_host": os.environ["TESTING_DATABASE_HOST"],
+        "database_name": os.environ["TESTING_DATABASE_NAME"],
+    },
+    "production": {
+        "database_user": os.environ["DATABASE_USER"],
+        "database_password": os.environ["DATABASE_PASSWORD"],
+        "database_host": os.environ["DATABASE_HOST"],
+        "database_name": os.environ["DATABASE_NAME"],
+    },
+}
 
-engine = create_engine(connection_uri, pool_size=50, max_overflow=50)
-Session = sessionmaker(bind=engine)
-Base = declarative_base()
+current_env = os.environ.get("DB_ENV", "production")
+connection_params = database_connections.get(current_env)
+
+if connection_params:
+    connection_uri = sa.engine.URL.create(
+        "postgresql+psycopg2",
+        username=connection_params["database_user"],
+        password=connection_params["database_password"],
+        host=connection_params["database_host"],
+        database=connection_params["database_name"],
+    )
+
+    engine = create_engine(connection_uri, pool_size=50, max_overflow=50)
+    Session = sessionmaker(bind=engine)
+    Base = declarative_base()
+else:
+    # Handle the case where the some_env value is not recognized
+    raise ValueError(f"Unknown environment: {current_env}")
