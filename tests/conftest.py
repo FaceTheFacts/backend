76791f32e368fd4a9e4d6218227f1656eca37716
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2023-11-16 14:29:37 +0100

    feat: Test step1

--- tests/conftest.py
+++ tests/conftest.py
@@ -89,6 +89,136 @@ def postgres_session(postgres_db):
 
 # Arrange
 @pytest.fixture
+def setup_sqlite_party_related_tables(session):
+    """Create a postgres session with party related tables for testing."""
+    # PartyStyle
+    party_style_data = [
+        (1, "CDU", "#000000", "#FFFFFF", "#000000"),
+        (2, "SPD", "#000000", "#FFFFFF", "#000000"),
+        (3, "FDP", "#000000", "#FFFFFF", "#000000"),
+        (4, "AfD", "#000000", "#FFFFFF", "#000000"),
+    ]
+    party_styles = [
+        PartyStyle(
+            id=id,
+            display_name=display_name,
+            foreground_color=foreground_color,
+            background_color=background_color,
+            border_color=border_color,
+        )
+        for id, display_name, foreground_color, background_color, border_color in party_style_data
+    ]
+    session.add_all(party_styles)
+    session.commit()
+
+    # Party
+    party_data = [
+        (
+            1,
+            "party",
+            "CDU",
+            "https://www.abgeordnetenwatch.de/api/parties/1",
+            "Christlich Demokratische Union Deutschlands",
+            "CDU",
+            1,
+        ),
+        (
+            2,
+            "party",
+            "SPD",
+            "https://www.abgeordnetenwatch.de/api/parties/2",
+            "Sozialdemokratische Partei Deutschlands",
+            "SPD",
+            2,
+        ),
+        (
+            3,
+            "party",
+            "FDP",
+            "https://www.abgeordnetenwatch.de/api/parties/3",
+            "Freie Demokratische Partei",
+            "FDP",
+            3,
+        ),
+        (
+            4,
+            "party",
+            "AfD",
+            "https://www.abgeordnetenwatch.de/api/parties/4",
+            "Alternative für Deutschland",
+            "AfD",
+            4,
+        ),
+    ]
+    parties = [
+        Party(
+            id=id,
+            entity_type=entity_type,
+            label=label,
+            api_url=api_url,
+            full_name=full_name,
+            short_name=short_name,
+            party_style_id=party_style_id,
+        )
+        for id, entity_type, label, api_url, full_name, short_name, party_style_id in party_data
+    ]
+    session.add_all(parties)
+    session.commit()
+
+    # PartyDonationOrganization
+    party_donation_organization_data = [
+        (1, "Stefan Quandt", "Seedammweg 55", "61352", "Bad Hamburg", False),
+        (2, "Susanne Klatten", "Seedammweg 55", "61352", "Bad Hamburg", False),
+        (3, "Friede Springer", "Axel-Springer-Str. 65", "10888", "Berlin", False),
+        (4, "Friede Springer", "Axel-Springer-Str. 65", "10888", "Berlin", False),
+    ]
+    party_donation_organizations = [
+        PartyDonationOrganization(
+            id=id,
+            donor_name=donor_name,
+            donor_address=donor_address,
+            donor_zip=donor_zip,
+            donor_city=donor_city,
+            donor_foreign=donor_foreign,
+        )
+        for id, donor_name, donor_address, donor_zip, donor_city, donor_foreign in party_donation_organization_data
+    ]
+    session.add_all(party_donation_organizations)
+    session.commit()
+
+    # PartyDonation
+    party_donation_data = [
+        (1, 1000.0, date(2020, 1, 1), 1, 1),
+        (2, 1000.0, date(2021, 1, 1), 1, 2),
+        (3, 1000.0, date(2022, 1, 1), 2, 3),
+        (4, 1000.0, date(2023, 1, 1), 2, 4),
+    ]
+    party_donations = [
+        PartyDonation(
+            id=id,
+            amount=amount,
+            date=d,
+            party_id=party_id,
+            party_donation_organization_id=party_donation_organization_id,
+        )
+        for id, amount, d, party_id, party_donation_organization_id in party_donation_data
+    ]
+    session.add_all(party_donations)
+    session.commit()
+
+    yield session
+
+    # Cleanup
+    session.query(PartyDonation).delete()
+    session.query(PartyDonationOrganization).delete()
+    session.query(Party).delete()
+    session.query(PartyStyle).delete()
+    session.commit()
+    session.close()
+
+
+# Arrange
+@pytest.fixture
 def setup_postgres_party_related_tables(postgres_session):
     """Create a postgres session with party related tables for testing."""
     # PartyStyle
--- tests/unit/service_layer/test_handlers.py
+++ tests/unit/service_layer/test_handlers.py
@@ -3,187 +3,205 @@ from sqlalchemy import text
 
 # local
 from src.service_layer import handlers, messagebus, utils
-from src.domain import events
+from src.domain import events, commands
 from src.api import repository
 
-
 class TestHandlers:
-    def test_prepare_update_data(self):
-        # Arrange
-        data = [
-            {
-                "id": 1,
-                "entity_type": "party",
-                "label": "SPD",
-                "api_url": "https://www.abgeordnetenwatch.de/api/parties/1",
-                "full_name": "Sozialdemokratische Partei Deutschlands",
-                "short_name": "SPD",
-            },
-            {
-                "id": 2,
-                "entity_type": "party",
-                "label": "CDU",
-                "api_url": "https://www.abgeordnetenwatch.de/api/parties/2",
-                "full_name": "Christlich Demokratische Union Deutschlands",
-                "short_name": "CDU",
-            },
-        ]
-        event = events.MissingEntityFetched(entity="party", data=data)
-        # Act
-        result = handlers.prepare_update_data(event)
-        # Assert
-        assert result == {
-            "entities": ["party_style", "party"],
-            "data": [
-                [
-                    {
-                        "id": 1,
-                        "display_name": "SPD",
-                        "foreground_color": "#FFFFFF",
-                        "background_color": "#333333",
-                        "border_color": None,
-                    },
-                    {
-                        "id": 2,
-                        "display_name": "CDU",
-                        "foreground_color": "#FFFFFF",
-                        "background_color": "#333333",
-                        "border_color": None,
-                    },
-                ],
-                [
-                    {
-                        "id": 1,
-                        "entity_type": "party",
-                        "label": "SPD",
-                        "api_url": "https://www.abgeordnetenwatch.de/api/parties/1",
-                        "full_name": "Sozialdemokratische Partei Deutschlands",
-                        "short_name": "SPD",
-                        "party_style_id": 1,
-                    },
-                    {
-                        "id": 2,
-                        "entity_type": "party",
-                        "label": "CDU",
-                        "api_url": "https://www.abgeordnetenwatch.de/api/parties/2",
-                        "full_name": "Christlich Demokratische Union Deutschlands",
-                        "short_name": "CDU",
-                        "party_style_id": 2,
-                    },
-                ],
-            ],
-        }
+    """ Step1: Fetch missing party data from abgeordnetenwatch.de """
+    def test_fetch_missing_party_with_no_party_exist(self, session):
+        missing_party_data = messagebus.handle(
+            commands.FetchMissingEntity(entity="party", session=session)
+        )
+        messagebus.handle(events.MissingEntityFetched(entity="party"))
+        assert missing_party_data != []
 
-    def test_update_table(self, session):
-        # Arrange
-        entities = ["party_style", "party"]
-        data = [
-            [
-                {
-                    "id": 1,
-                    "display_name": "SPD",
-                    "foreground_color": "#000000",
-                    "background_color": "#FFFFFF",
-                    "border_color": "#000000",
-                },
-                {
-                    "id": 2,
-                    "display_name": "SPD",
-                    "foreground_color": "#000000",
-                    "background_color": "#FFFFFF",
-                    "border_color": "#000000",
-                },
-            ],
-            [
-                {
-                    "id": 1,
-                    "entity_type": "party",
-                    "label": "SPD",
-                    "api_url": "https://www.abgeordnetenwatch.de/api/parties/1",
-                    "full_name": "Sozialdemokratische Partei Deutschlands",
-                    "short_name": "SPD",
-                    "party_style_id": 1,
-                },
-                {
-                    "id": 2,
-                    "entity_type": "party",
-                    "label": "CDU",
-                    "api_url": "https://www.abgeordnetenwatch.de/api/parties/2",
-                    "full_name": "Christlich Demokratische Union Deutschlands",
-                    "short_name": "CDU",
-                    "party_style_id": 2,
-                },
-            ],
-        ]
-        event = events.UpdatedEntityPrepared(
-            entities=entities, data=data, session=session
+    """ Step1: Fetch missing party data from abgeordnetenwatch.de"""
+    def test_fetch_missing_party_with_party_exist(
+        self, session, setup_sqlite_party_related_tables
+    ):
+        missing_party_data = messagebus.handle(
+            commands.FetchMissingEntity(entity="party", session=session)
         )
-        # Act
-        handlers.update_table(event)
-        rows = session.execute(
-            text("SELECT * FROM party_style WHERE id = :id"), {"id": 1}
-        )
-        # Assert
-        assert list(rows) == [
-            (
-                1,
-                "SPD",
-                "#000000",
-                "#FFFFFF",
-                "#000000",
-            )
-        ]
-        # Act
-        rows = session.execute(text("SELECT * FROM party WHERE id = :id"), {"id": 1})
-        # Assert
-        assert list(rows) == [
-            (
-                1,
-                "party",
-                "SPD",
-                "https://www.abgeordnetenwatch.de/api/parties/1",
-                "Sozialdemokratische Partei Deutschlands",
-                "SPD",
-                1,
-            )
-        ]
-        # Cleanup
-        session.execute(text("DELETE FROM party_style"))
-        session.execute(text("DELETE FROM party"))
+        messagebus.handle(events.MissingEntityFetched(entity="party"))
+        assert missing_party_data != []
+        
 
-    def test_fetch_missing_party_update_table(self, session):
-        factory = repository.SqlAlchemyFactory(session)
-        repo = factory.create_party_repository()
-        missing_party_data = utils.FetchMissingEntity(
-            "parties", repo
-        ).fetch_missing_entities()
-        update_data = messagebus.handle(
-            events.MissingEntityFetched(entity="party", data=missing_party_data)  # type: ignore
-        )
-        messagebus.handle(
-            events.UpdatedEntityPrepared(
-                **update_data[0], session=session  # type: ignore
-            )
-        )  # type: ignore
-        rows = session.execute(
-            text("SELECT * FROM party_style WHERE id = :id"), {"id": 1}
-        )
-        # Assert
-        assert list(rows) == [(1, "SPD", "#000000", "#FFFFFF", "#000000")]
-        # Act
-        rows = session.execute(text("SELECT * FROM party WHERE id = :id"), {"id": 1})
-        # # Assert
-        assert list(rows) == [
-            (
-                1,
-                "party",
-                "SPD",
-                "https://www.abgeordnetenwatch.de/api/parties/1",
-                "Sozialdemokratische Partei Deutschlands",
-                "SPD",
-                1,
-            )
-        ]
-        # Cleanup
-        session.execute(text("DELETE FROM party_style"))
-        session.execute(text("DELETE FROM party"))
+    # def test_prepare_update_data(self):
+    #     # Arrange
+    #     data = [
+    #         {
+    #             "id": 1,
+    #             "entity_type": "party",
+    #             "label": "SPD",
+    #             "api_url": "https://www.abgeordnetenwatch.de/api/parties/1",
+    #             "full_name": "Sozialdemokratische Partei Deutschlands",
+    #             "short_name": "SPD",
+    #         },
+    #         {
+    #             "id": 2,
+    #             "entity_type": "party",
+    #             "label": "CDU",
+    #             "api_url": "https://www.abgeordnetenwatch.de/api/parties/2",
+    #             "full_name": "Christlich Demokratische Union Deutschlands",
+    #             "short_name": "CDU",
+    #         },
+    #     ]
+    #     event = events.MissingEntityFetched(entity="party", data=data)
+    #     # Act
+    #     result = handlers.prepare_update_data(event)
+    #     # Assert
+    #     assert result == {
+    #         "entities": ["party_style", "party"],
+    #         "data": [
+    #             [
+    #                 {
+    #                     "id": 1,
+    #                     "display_name": "SPD",
+    #                     "foreground_color": "#FFFFFF",
+    #                     "background_color": "#333333",
+    #                     "border_color": None,
+    #                 },
+    #                 {
+    #                     "id": 2,
+    #                     "display_name": "CDU",
+    #                     "foreground_color": "#FFFFFF",
+    #                     "background_color": "#333333",
+    #                     "border_color": None,
+    #                 },
+    #             ],
+    #             [
+    #                 {
+    #                     "id": 1,
+    #                     "entity_type": "party",
+    #                     "label": "SPD",
+    #                     "api_url": "https://www.abgeordnetenwatch.de/api/parties/1",
+    #                     "full_name": "Sozialdemokratische Partei Deutschlands",
+    #                     "short_name": "SPD",
+    #                     "party_style_id": 1,
+    #                 },
+    #                 {
+    #                     "id": 2,
+    #                     "entity_type": "party",
+    #                     "label": "CDU",
+    #                     "api_url": "https://www.abgeordnetenwatch.de/api/parties/2",
+    #                     "full_name": "Christlich Demokratische Union Deutschlands",
+    #                     "short_name": "CDU",
+    #                     "party_style_id": 2,
+    #                 },
+    #             ],
+    #         ],
+    #     }
+
+    # def test_update_table(self, session):
+    #     # Arrange
+    #     entities = ["party_style", "party"]
+    #     data = [
+    #         [
+    #             {
+    #                 "id": 1,
+    #                 "display_name": "SPD",
+    #                 "foreground_color": "#000000",
+    #                 "background_color": "#FFFFFF",
+    #                 "border_color": "#000000",
+    #             },
+    #             {
+    #                 "id": 2,
+    #                 "display_name": "SPD",
+    #                 "foreground_color": "#000000",
+    #                 "background_color": "#FFFFFF",
+    #                 "border_color": "#000000",
+    #             },
+    #         ],
+    #         [
+    #             {
+    #                 "id": 1,
+    #                 "entity_type": "party",
+    #                 "label": "SPD",
+    #                 "api_url": "https://www.abgeordnetenwatch.de/api/parties/1",
+    #                 "full_name": "Sozialdemokratische Partei Deutschlands",
+    #                 "short_name": "SPD",
+    #                 "party_style_id": 1,
+    #             },
+    #             {
+    #                 "id": 2,
+    #                 "entity_type": "party",
+    #                 "label": "CDU",
+    #                 "api_url": "https://www.abgeordnetenwatch.de/api/parties/2",
+    #                 "full_name": "Christlich Demokratische Union Deutschlands",
+    #                 "short_name": "CDU",
+    #                 "party_style_id": 2,
+    #             },
+    #         ],
+    #     ]
+    #     event = events.UpdatedEntityPrepared(
+    #         entities=entities, data=data, session=session
+    #     )
+    #     # Act
+    #     handlers.update_table(event)
+    #     rows = session.execute(
+    #         text("SELECT * FROM party_style WHERE id = :id"), {"id": 1}
+    #     )
+    #     # Assert
+    #     assert list(rows) == [
+    #         (
+    #             1,
+    #             "SPD",
+    #             "#000000",
+    #             "#FFFFFF",
+    #             "#000000",
+    #         )
+    #     ]
+    #     # Act
+    #     rows = session.execute(text("SELECT * FROM party WHERE id = :id"), {"id": 1})
+    #     # Assert
+    #     assert list(rows) == [
+    #         (
+    #             1,
+    #             "party",
+    #             "SPD",
+    #             "https://www.abgeordnetenwatch.de/api/parties/1",
+    #             "Sozialdemokratische Partei Deutschlands",
+    #             "SPD",
+    #             1,
+    #         )
+    #     ]
+    #     # Cleanup
+    #     session.execute(text("DELETE FROM party_style"))
+    #     session.execute(text("DELETE FROM party"))
+
+    # def test_fetch_missing_party_update_table(self, session):
+    #     factory = repository.SqlAlchemyFactory(session)
+    #     repo = factory.create_party_repository()
+    #     missing_party_data = utils.FetchMissingEntity(
+    #         "parties", repo
+    #     ).fetch_missing_entities()
+    #     update_data = messagebus.handle(
+    #         events.MissingEntityFetched(entity="party", data=missing_party_data)  # type: ignore
+    #     )
+    #     messagebus.handle(
+    #         events.UpdatedEntityPrepared(
+    #             **update_data[0], session=session  # type: ignore
+    #         )
+    #     )  # type: ignore
+    #     rows = session.execute(
+    #         text("SELECT * FROM party_style WHERE id = :id"), {"id": 1}
+    #     )
+    #     # Assert
+    #     assert list(rows) == [(1, "SPD", "#000000", "#FFFFFF", "#000000")]
+    #     # Act
+    #     rows = session.execute(text("SELECT * FROM party WHERE id = :id"), {"id": 1})
+    #     # # Assert
+    #     assert list(rows) == [
+    #         (
+    #             1,
+    #             "party",
+    #             "SPD",
+    #             "https://www.abgeordnetenwatch.de/api/parties/1",
+    #             "Sozialdemokratische Partei Deutschlands",
+    #             "SPD",
+    #             1,
+    #         )
+    #     ]
+    #     # Cleanup
+    #     session.execute(text("DELETE FROM party_style"))
+    #     session.execute(text("DELETE FROM party"))
