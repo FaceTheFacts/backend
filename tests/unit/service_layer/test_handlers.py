a87b2e9417c06582329592bd12f623a54c2a8133
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2023-11-16 15:47:00 +0100

    feat: Test step2

--- tests/unit/service_layer/test_handlers.py
+++ tests/unit/service_layer/test_handlers.py
@@ -6,25 +6,101 @@ from src.service_layer import handlers, messagebus, utils
 from src.domain import events, commands
 from src.api import repository
 
+
 class TestHandlers:
-    """ Step1: Fetch missing party data from abgeordnetenwatch.de """
+    """Step1: Fetch missing party data from abgeordnetenwatch.de"""
+
     def test_fetch_missing_party_with_no_party_exist(self, session):
+        # Act
         missing_party_data = messagebus.handle(
             commands.FetchMissingEntity(entity="party", session=session)
         )
         messagebus.handle(events.MissingEntityFetched(entity="party"))
+        # Assert
         assert missing_party_data != []
 
     """ Step1: Fetch missing party data from abgeordnetenwatch.de"""
+
     def test_fetch_missing_party_with_party_exist(
         self, session, setup_sqlite_party_related_tables
     ):
+        # Act
         missing_party_data = messagebus.handle(
             commands.FetchMissingEntity(entity="party", session=session)
         )
         messagebus.handle(events.MissingEntityFetched(entity="party"))
+        # Assert
         assert missing_party_data != []
-        
+
+    """Step2: Prepare data for updating party_style and party tables"""
+
+    def test_prepare_update_data(self):
+        # Arrange
+        data = [
+            {
+                "id": 1,
+                "entity_type": "party",
+                "label": "SPD",
+                "api_url": "https://www.abgeordnetenwatch.de/api/parties/1",
+                "full_name": "Sozialdemokratische Partei Deutschlands",
+                "short_name": "SPD",
+            },
+            {
+                "id": 2,
+                "entity_type": "party",
+                "label": "CDU",
+                "api_url": "https://www.abgeordnetenwatch.de/api/parties/2",
+                "full_name": "Christlich Demokratische Union Deutschlands",
+                "short_name": "CDU",
+            },
+        ]
+        # Act
+        results = messagebus.handle(
+            commands.PrepareUpdateData(entity="party", data=data)
+        )
+        messagebus.handle(events.UpdatedEntityPrepared(entities=results[0]["entities"]))  # type: ignore
+        # Assert
+        assert results[0] == {
+            "entities": ["party_style", "party"],
+            "data": [
+                [
+                    {
+                        "id": 1,
+                        "display_name": "SPD",
+                        "foreground_color": "#FFFFFF",
+                        "background_color": "#333333",
+                        "border_color": None,
+                    },
+                    {
+                        "id": 2,
+                        "display_name": "CDU",
+                        "foreground_color": "#FFFFFF",
+                        "background_color": "#333333",
+                        "border_color": None,
+                    },
+                ],
+                [
+                    {
+                        "id": 1,
+                        "entity_type": "party",
+                        "label": "SPD",
+                        "api_url": "https://www.abgeordnetenwatch.de/api/parties/1",
+                        "full_name": "Sozialdemokratische Partei Deutschlands",
+                        "short_name": "SPD",
+                        "party_style_id": 1,
+                    },
+                    {
+                        "id": 2,
+                        "entity_type": "party",
+                        "label": "CDU",
+                        "api_url": "https://www.abgeordnetenwatch.de/api/parties/2",
+                        "full_name": "Christlich Demokratische Union Deutschlands",
+                        "short_name": "CDU",
+                        "party_style_id": 2,
+                    },
+                ],
+            ],
+        }
 
     # def test_prepare_update_data(self):
     #     # Arrange
