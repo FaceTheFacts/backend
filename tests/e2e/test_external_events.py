d364a9ed0e222a542e13fca4d73b77e66c9f8bc3
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2023-12-06 11:47:55 +0100

    feat: Add a test for pipeline

---
+++ tests/e2e/test_external_events.py
@@ -0,0 +1,127 @@
+# std
+import json
+from sqlalchemy import text
+
+# third-party
+from tenacity import Retrying, RetryError, stop_after_delay
+
+# local
+from src.entrypoints import (
+    redis_utils,
+    redis_eventpublisher,
+    redis_eventconsumer_missing_entity_fetched,
+    redis_eventconsumer_update_data_prepared,
+)
+from src.db.models.party import Party
+
+
+class TestPipeline:
+    def test_fetch_missing_entity_from_third_party_api(self, session):
+        redis_client = redis_utils.RedisClient(host="localhost", port=6379)
+        pubsub = redis_client.pubsub()
+        # Arrange
+        pubsub.subscribe("missing_entity_fetched")  # Subscribe to channel 1
+        pubsub.subscribe("updated_entity_prepared")  # Subscribe to channel 2
+
+        # Act
+        entity = "party"
+        redis_eventpublisher.initiate_fetch_missing_data(
+            entity=entity, session=session, redis_client=redis_client
+        )
+
+        messages = []
+        for attempt in Retrying(stop=stop_after_delay(3), reraise=True):
+            with attempt:
+                message = pubsub.get_message(timeout=1)
+                if message:
+                    messages.append(message)
+                    print(message)
+                data = json.loads(messages[-1]["data"])
+                assert data["entity"] == entity
+
+                # if message:
+                #     messages.append(message)
+                #     data = json.loads(messages[-1]["data"])
+                #     assert data == ""
+                # assert data["entities"] == ["party_style", "party"]
+                # assert data["data"] == 2
+
+            #         redis_eventconsumer_missing_entity_fetched.handle_message(
+            #             message=message_from_channel_one
+            #         )
+            #         message_from_channel_two = pubsub.get_message(timeout=1)
+            #         assert message_from_channel_two ==[]
+            #         messages.append(message_from_channel_two)
+            #         if message_from_channel_two:
+            #             redis_eventconsumer_update_data_prepared.handle_message(
+            #                 message=message_from_channel_two, session=session
+            #             )
+            # return messages
+        # Assert
+        # assert messages == []
+        # data = json.loads(messages[-1]["data"])
+        # assert data["entities"] == [entity, "party_style"]
+        # assert len(data["data"]) == 2
+        # rows = session.execute(text("SELECT * FROM party WHERE id = :id"), {"id": 8})
+        # assert list(rows) == [""]
+
+    # def test_update_party_related_table_with_mock_data(self, session):
+    #     redis_client = RedisClient(host="localhost", port=6379)
+    #     pubsub = redis_client.pubsub()
+    #     pubsub.subscribe("updated_entity_prepared")
+    #     # Arrange
+    #     entities = ["party_style", "party"]
+    #     data = [
+    #         [
+    #             {
+    #                 "id": 1,
+    #                 "display_name": "SPD",
+    #                 "foreground_color": "#000000",
+    #                 "background_color": "#FFFFFF",
+    #                 "border_color": "#000000",
+    #             },
+    #             {
+    #                 "id": 2,
+    #                 "display_name": "SPD",
+    #                 "foreground_color": "#000000",
+    #                 "background_color": "#FFFFFF",
+    #                 "border_color": "#000000",
+    #             },
+    #         ],
+    #         [
+    #             {
+    #                 "id": 1,
+    #                 "entity_type": "party",
+    #                 "label": "SPD",
+    #                 "api_url": "https://www.abgeordnetenwatch.de/api/parties/1",
+    #                 "full_name": "Sozialdemokratische Partei Deutschlands",
+    #                 "short_name": "SPD",
+    #                 "party_style_id": 1,
+    #             },
+    #             {
+    #                 "id": 2,
+    #                 "entity_type": "party",
+    #                 "label": "CDU",
+    #                 "api_url": "https://www.abgeordnetenwatch.de/api/parties/2",
+    #                 "full_name": "Christlich Demokratische Union Deutschlands",
+    #                 "short_name": "CDU",
+    #                 "party_style_id": 2,
+    #             },
+    #         ],
+    #     ]
+    #     # Act
+    #     redis_client.publish(
+    #         channel="updated_entity_prepared",
+    #         message=json.dumps({"entities": entities, "data": data}),
+    #     )
+    #     # Assert
+    #     messages = []
+    #     for attempt in Retrying(stop=stop_after_delay(3), reraise=True):
+    #         with attempt:
+    #             message = pubsub.get_message(timeout=1)
+    #             if message:
+    #                 messages.append(message)
+    #                 print(message)
+    #             data = json.loads(messages[-1]["data"])
+    #             assert data["entities"] == ["party_style", "party"]
+    #             assert len(data["data"]) == 2
