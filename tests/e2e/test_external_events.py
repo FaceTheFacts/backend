1bfe5c5d2b77c16cabb24bc9013d85a2b6530e63
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2023-12-06 15:46:15 +0100

    feat: Refactor tests

--- tests/e2e/test_external_events.py
+++ tests/e2e/test_external_events.py
@@ -1,7 +1,10 @@
 # std
 import json
 import time
+
+# third-party
 from sqlalchemy import text
+import pytest
 
 # local
 from src.entrypoints import (
@@ -23,53 +26,59 @@ def wait_for_messages(pubsub, timeout):
     return messages
 
 
+# Turn on redis-server before running the tests
+@pytest.mark.e2e
 class TestPipeline:
+    """Test the Redis event pipeline."""
+
+    def setup_method(self):
+        """Setup the Redis event pipeline."""
+        self.redis_client = redis_utils.RedisClient(host="localhost", port=6379)
+        self.pubsub_missing_entity = self.redis_client.pubsub()
+        self.pubsub_updated_entity = self.redis_client.pubsub()
+
+        self.pubsub_missing_entity.subscribe("missing_entity_fetched")
+        self.pubsub_updated_entity.subscribe("updated_entity_prepared")
+
+    def cleanup(self):
+        """Cleanup the Redis event pipeline."""
+        self.session.execute(text("DELETE FROM party_style"))
+        self.session.execute(text("DELETE FROM party"))
+
     def test_fetch_missing_entity_from_third_party_api(self, session):
-        redis_client = redis_utils.RedisClient(host="localhost", port=6379)
-        pubsub = redis_client.pubsub()
-        # Arrange
-        pubsub.subscribe("missing_entity_fetched")  # Subscribe to channel 1
-
-        # Act
+        """Test fetching missing entity from third-party API. (step 1)"""
+        self.session = session
         entity = "party"
+        # Act
         redis_eventpublisher.initiate_fetch_missing_data(
-            entity=entity, session=session, redis_client=redis_client
+            entity=entity, session=session, redis_client=self.redis_client
         )
-        messages = wait_for_messages(pubsub, timeout=3)
-        data = json.loads(messages[-1]["data"])
 
+        messages = wait_for_messages(self.pubsub_missing_entity, timeout=3)
+        data = json.loads(messages[-1]["data"])
         # Assert
         assert data["entity"] == entity
         assert len(data["data"]) != 0
 
     def test_subscribe_updated_entity_prepared(self, session):
-        redis_client = redis_utils.RedisClient(host="localhost", port=6379)
-
-        # Create separate pubsub instances for each channel
-        pubsub_missing_entity = redis_client.pubsub()
-        pubsub_updated_entity = redis_client.pubsub()
-
-        # Arrange
-        pubsub_missing_entity.subscribe("missing_entity_fetched")
-        pubsub_updated_entity.subscribe("updated_entity_prepared")
-
-        # Act
+        """Test subscribing to updated_entity_prepared event. (step 1 and step 2)"""
+        self.session = session
         entity = "party"
-
-        # Publish messages to the "missing_entity_fetched" channel
+        # Act
         redis_eventpublisher.initiate_fetch_missing_data(
-            entity=entity, session=session, redis_client=redis_client
+            entity=entity, session=session, redis_client=self.redis_client
         )
 
-        # Wait for messages on the "missing_entity_fetched" channel
-        messages_missing_entity = wait_for_messages(pubsub_missing_entity, timeout=3)
+        messages_missing_entity = wait_for_messages(
+            self.pubsub_missing_entity, timeout=3
+        )
 
-        # Handle messages on the "missing_entity_fetched" channel
         for message in messages_missing_entity:
             redis_eventconsumer_missing_entity_fetched.handle_message(message=message)
 
-        # Wait for messages on the "updated_entity_prepared" channel
-        messages_updated_entity = wait_for_messages(pubsub_updated_entity, timeout=3)
+        messages_updated_entity = wait_for_messages(
+            self.pubsub_updated_entity, timeout=3
+        )
 
         data = json.loads(messages_updated_entity[-1]["data"])
         # Assert
@@ -77,41 +86,32 @@ class TestPipeline:
         assert len(data["data"]) == 2
 
     def test_update_data_with_third_party_api_data(self, session):
-        redis_client = redis_utils.RedisClient(host="localhost", port=6379)
-
-        # Create separate pubsub instances for each channel
-        pubsub_missing_entity = redis_client.pubsub()
-        pubsub_updated_entity = redis_client.pubsub()
-
-        # Arrange
-        pubsub_missing_entity.subscribe("missing_entity_fetched")
-        pubsub_updated_entity.subscribe("updated_entity_prepared")
-
-        # Act
+        """Test updating data with third-party API data. (step 1, step 2 and step 3)"""
+        self.session = session
         entity = "party"
-
-        # Publish messages to the "missing_entity_fetched" channel
+        # Act
         redis_eventpublisher.initiate_fetch_missing_data(
-            entity=entity, session=session, redis_client=redis_client
+            entity=entity, session=session, redis_client=self.redis_client
         )
 
-        # Wait for messages on the "missing_entity_fetched" channel
-        messages_missing_entity = wait_for_messages(pubsub_missing_entity, timeout=3)
+        messages_missing_entity = wait_for_messages(
+            self.pubsub_missing_entity, timeout=3
+        )
 
-        # Handle messages on the "missing_entity_fetched" channel
         for message in messages_missing_entity:
             redis_eventconsumer_missing_entity_fetched.handle_message(message=message)
 
-        # Wait for messages on the "updated_entity_prepared" channel
-        messages_updated_entity = wait_for_messages(pubsub_updated_entity, timeout=3)
+        messages_updated_entity = wait_for_messages(
+            self.pubsub_updated_entity, timeout=3
+        )
 
         for message in messages_updated_entity:
             redis_eventconsumer_update_data_prepared.handle_message(
                 message=message, session=session
             )
 
-        # Assert
         rows = session.execute(text("SELECT * FROM party WHERE id = :id"), {"id": 220})
+        # Assert
         assert list(rows) == [
             (
                 220,
@@ -124,5 +124,4 @@ class TestPipeline:
             )
         ]
         # Cleanup
-        session.execute(text("DELETE FROM party_style"))
-        session.execute(text("DELETE FROM party"))
+        self.cleanup()
